3c46459326c64daafc11b504c0932ccfaae927c8
Rheno Budiasa <rhenobudiasa@yahoo.com>
2013-10-18 09:24:36 +0700

    Fix Some bug

--- Greedy-Algorithm/greedy.c
+++ Greedy-Algorithm/greedy.c
@@ -10,10 +10,10 @@
 #include<stdio.h>
 
 /* define how many input array */
-#define N 5
+#define N 4
 
 /* define how many change will get */ 
-#define G 100
+#define G 32
 
 
 /* Greedy algorithm function */
@@ -21,18 +21,24 @@ void greedy(int input[]){
   int i,j,total=G, coin=0, find=0;
     for(i=0;i<N;i++){
       /* find rest of the coin */
-      total = total - input[i];
-     
-      /* sum of coin */
-      coin++;
-
+      if(total - input[i] >=0){
+        
+	/* Total rest of coin */
+       total = total - input[i];
+ 
+       /* Increment solution coin */
+       coin++;
+      }
+      
+             
       /* check rest of coin from current input item (solution) */
-      if(total%input[i] == 0){
+      if(total%input[i] == 0){        
         coin = coin + (total/input[i]);
         find = 1;
         printf("Solution = %d coin",coin);
 	return;
       }
+      
     }
 
     /* if no solution found */
@@ -46,7 +52,7 @@ void greedy(int input[]){
 int main(){
   
   /* input Array */
-  int input[] = {50,40,30,20,10};
+  int input[] = {25,10,5,1};
   
   /* greedy function implementation */  
   greedy(input);
