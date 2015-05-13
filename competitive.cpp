49d72d5b77fd8569d34aa72a54e386e1f622a727
rhenobudiasa <rhenobudiasa@yahoo.com>
2015-05-13 23:39:23 +0700

    competitive.cpp

---
+++ competitive.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <utility>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+
+using namespace std;
+
+
+const int INF = 1000000000;
+
+
+typedef long long ll;
+typedef long l;	
+typedef vector<int> vi;
+typedef vector<l> vl;
+typedef vector< vi > vii; 
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+
+#define SZ(a) int((a).size())
+#define PB push_back
+#define MP make_pair
+#define ALL(c) (c).begin(), (c).end()
+#define IT(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define TR(i,a,b) for(int (i)=a;(i)<b;(i)++)
+#define PRESENT(c,x) ((c).find(x) != (c).end()) 
+#define CPRESENT(c,x) (find(ALL(c),x) != (c).end()) 
+
+
+
+int main(){
+
+
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	
+
+    
+	return 0;
+}
